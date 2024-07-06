8d0c53cebe55f4820a892cf234d285ebc76bd8de
Hendrik Yong <hendrikyong1205@gmail.com>
2024-07-07 02:18:26 +0800

    leaderboard.js

--- fed.js
+++ fed.js
@@ -1,13 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
-  const url = "https://pookiebears-04f9.restdb.io/rest/spmleaderboard";
-  const apikey = "6686c097e0ddd887ed0940e1";
-  //const url = "https://feddddd-6882.restdb.io/rest/assignment2leaderboard";
-  //const apikey = "65c4358c86354f3586464a0d";
+  // const url = "https://pookiebears-04f9.restdb.io/rest/spmleaderboard";
+  // const apikey = "6686c097e0ddd887ed0940e1";
+  const url = "https://feddddd-6882.restdb.io/rest/assignment2leaderboard";
+  const apikey = "65c4358c86354f3586464a0d";
 
   getData();
 
-  //retrieve high score from db
+  document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById("username").value;
+    let highScore = localStorage.getItem("high-score");
+
+    // Check if the user has entered a name and a score
+    if (name && highScore > 0) {
+      // Check and update high score if necessary
+      getHighScore(name, highScore);
+
+      // Reset local storage for high score only if it's a new name
+      let existingData =
+        JSON.parse(localStorage.getItem("high-score-data")) || {};
+      if (!existingData[name]) {
+        localStorage.removeItem("high-score");
+      }
+
+      let jsondata = {
+        name: name,
+        score: highScore,
+      };
+
+      //send to db for the leaderboard
+      let settings = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-apikey": apikey,
+          "cache-control": "no-cache",
+        },
+        body: JSON.stringify(jsondata),
+      };
+
+      fetch(url, settings)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          document.getElementById("submit").disabled = false;
+          // Refresh leaderboard after posting data
+          getData();
+        })
+        .catch((error) => console.error("Error posting score: ", error));
+    } else {
+      // Handle case where user hasn't played the game yet
+      console.log("Please play the game before submitting your score.");
+    }
+  });
+
+  //get from db
   function getHighScore(name, highScore) {
     let settings = {
       method: "GET",
@@ -49,7 +97,6 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  //get data from db
   function getData(limit = 10) {
     let settings = {
       method: "GET",
